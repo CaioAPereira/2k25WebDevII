90940e56bc22a01ec356962eaa60d237a0489197
Caio A. Pereira <caioifsp1meirao@gmail.com>
2025-06-10 17:16:46 -0300

    bookshelf v1.0.1

--- bookshelf/routes/web.php
+++ bookshelf/routes/web.php
@@ -1,23 +1,11 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\EventController;
-
-
-Route::get('/', [EventController::class, 'index']);
-Route::get('/events/create', [EventController::class, 'create']);
-Route::get('/events/{id}', [EventController::class, 'show']);
-Route::post('/events', [EventController::class, 'store']);
-
-
-=======
 use App\Http\Controllers\LivroController;
 
 Route::get('/', [LivroController::class, 'index']);
-Route::get('/livros/create', [LivroController::class, 'create'])->middleware(
-    'auth'
-);
+Route::get('/livros/create', [LivroController::class, 'create'])->middleware('auth');
 Route::get('/livros/{id}', [LivroController::class, 'show']);
 Route::post('/livros', [LivroController::class, 'store']);
 Route::delete('/livros/{id}', [LivroController::class, 'destroy']);
@@ -42,7 +30,6 @@ Route::delete('/livros/leave/{id}', [
     LivroController::class,
     'leaveLivro',
 ])->middleware('auth');
->>>>>>> 98d9e50 (bookshelf v1.0.0)
 
 /*
 Route::get('produtos/', function () {
@@ -56,8 +43,4 @@ Route::get('produtos_teste/{id?}', function ($id = null) {
 
     return view('/product', ['id' => $id]);
 });
-<<<<<<< HEAD
 */
-=======
-*/
->>>>>>> 98d9e50 (bookshelf v1.0.0)
